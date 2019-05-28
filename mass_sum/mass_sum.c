d023d7e59188115ae538827fa216bae13aab4cee
Bob Robey <brobey@lanl.gov>
2019-05-27 21:32:35 -0600

    Removing hardcoded ncells

--- mass_sum/mass_sum.c
+++ mass_sum/mass_sum.c
@@ -1,12 +1,11 @@
 #include "mass_sum.h"
-#define NCELLS 10000000
 #define REAL_CELL 1
 
 double mass_sum(int ncells, int* restrict celltype,
                 double* restrict H, double* restrict dx, double* restrict dy){
    double summer = 0.0;
 #pragma omp simd reduction(+:summer)
-   for (int ic=0; ic<NCELLS ; ic++) {
+   for (int ic=0; ic<ncells ; ic++) {
       if (celltype[ic] == REAL_CELL) {
          summer += H[ic]*dx[ic]*dy[ic];
       }
