fda0d2970c4977d76d8097107eedb472aa9e2520
Bob Robey <brobey@lanl.gov>
2020-07-04 13:18:36 -0600

    Adding include guards

--- autovec/timer.h
+++ autovec/timer.h
@@ -1,5 +1,7 @@
-#ifndef TIMER_H
-#define TIMER_H
+#ifndef _TIMER_H
+#define _TIMER_H
+#include <time.h>
+
 void cpu_timer_start(struct timespec *tstart_cpu);
 double cpu_timer_stop(struct timespec tstart_cpu);
 #endif
--- mass_sum/mass_sum.h
+++ mass_sum/mass_sum.h
@@ -1,3 +1,7 @@
+#ifndef _MASS_SUM_H
+#define _MASS_SUM_H
+
 #define REAL_CELL 1
 
 double mass_sum(int ncells, int *celltype, double *H, double *dx, double *dy);
+#endif
