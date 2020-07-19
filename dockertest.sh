d44ca8ccc6854fa9ec0f3b5df3d89e914d0ad1f1
Bob Robey <brobey@lanl.gov>
2020-07-19 10:12:13 -0600

    Fixing docker and vm builds

--- dockertest.sh
+++ dockertest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#docker build --no-cache -t chapter6 .
+docker build --no-cache -t chapter6 .
 #docker run -it --entrypoint /bin/bash chapter6
 docker build --no-cache -f Dockerfile.Ubuntu20.04 -t chapter6 .
 #docker run -it --entrypoint /bin/bash chapter6
